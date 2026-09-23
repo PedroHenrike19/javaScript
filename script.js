d38b18bc8b1bf312b98fa96041ecea18cf529722
PedroHenrike19 <pedro.nascimento.faria@escola.pr.gov.br>
2026-09-23 11:08:00 +0000

    Alteração3

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarAlerta(){
+    alert(Bom dia Turma do 1ºAno)
+}
+
+function pedirNome(){
+    let nome = prompt("Informe o seu nome: ")
+    alert(Bom dia, " + nome)
+    }
+
+    function somarNumeros(){
+        let primeiroValor = Number(promt("Informe o 
